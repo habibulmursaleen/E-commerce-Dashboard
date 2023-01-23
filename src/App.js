d9adb5948c33e1433674f24f81b6aa78a694c155
habibulmursaleen <habibulmursaleen97@gmail.com>
2023-01-23 04:10:16 +0100

    commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
+import React from "react";
 import "./App.css";
 
-function App() {
-  return <div className="App"></div>;
-}
+const App = () => {
+  return <h1 className="underline text-3xl">App</h1>;
+};
 
 export default App;
